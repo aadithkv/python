30c3dcd06dcc08e84425ff5b63eeb114ba11e4dd
aadithkv <aadithkvijay.vijay94@gmail.com>
2018-06-07 17:00:04 +0000

    first commit

---
+++ sample.py
@@ -0,0 +1,14 @@
+mport random
+import os
+import sys
+
+test_file=open("test.txt","wb")
+print(test_file.mode)
+print(test_file.name)
+
+test_file.write(bytes("hai \n",'UTF-8'))
+test_file.close()
+test_file=open("test.txt","r+")
+text_in_file=test_file.read()
+print(text_in_file)
+# python
